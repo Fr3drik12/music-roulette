b7873d52aa05e9d2a1f92c12423cc06005028061
Fr3drik12 <161924867+Fr3drik12@users.noreply.github.com>
2024-06-28 17:31:51 +0200

    Add files via upload

---
+++ music_roulette.py
@@ -0,0 +1,24 @@
+import random
+import webbrowser
+
+num = random.randint(1,5)
+
+if num == 1:
+    kanden = "https://www.youtube.com/watch?v=UFQEttrn6CQ"
+    webbrowser.open(kanden)
+    
+if num == 2:
+    madheadlove = "https://www.youtube.com/watch?v=xCIJJ2Rb368"
+    webbrowser.open(madheadlove)
+    
+if num == 3:
+    shinagami = "https://www.youtube.com/watch?v=8nxaZ69ElEc"
+    webbrowser.open(shinagami)
+    
+if num == 4:
+    poppin_apathy = "https://www.youtube.com/watch?v=pUDI2x5Y0Wo"
+    webbrowser.open(poppin_apathy)
+    
+if num == 5:
+    lady = "https://www.youtube.com/watch?v=DdF-u3fe5pg"
+    webbrowser.open(lady)
